ba4b414a0e132c2675af09f142c6695dfcc9d90d
JHechtel <hechtel26@gmail.com>
2023-10-30 15:43:39 +0100

    Renaming and testing Git

---
+++ src/Exercise1/Car.java
@@ -0,0 +1,28 @@
+package Exercise1;
+
+class Car  {
+    final double milage;
+    final double oilcost;
+
+     Car(double milage, double oilcost) {
+        this.milage=milage;
+        this.oilcost=oilcost;
+    }
+
+    void cost(double distance){
+         double fuelcost= milage/100*distance;
+         double oilloss= oilcost/1000*distance;
+        System.out.println("Spritverbrauch: "+fuelcost+" Liter!");
+        System.out.println("Ölverbrauch: "+ oilloss+" Liter!");
+
+    }
+    void cost(double distance,double cost){
+        double fuelcost= (milage/100)*distance;
+        double oilloss= (oilcost/1000)*distance;
+        System.out.println("Spritverbrauch: "+fuelcost+" Liter!");
+        System.out.println("Ölverbrauch: "+ oilloss+" Liter!");
+        System.out.println("Für Sprit zahlen sie bei "+cost+"Euro pro Liter "+cost*fuelcost+"Euro!");
+
+    }
+
+}
---
+++ src/Exercise1/Main.java
@@ -0,0 +1,20 @@
+package Exercise1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bitte Verbrauch pro 100km angeben:");
+        double fuel= new Scanner(System.in).nextDouble();
+        System.out.println("Bitte Ölverbrauch pro 1000km angeben:");
+        double oil= new Scanner(System.in).nextDouble();
+        Car car = new Car(fuel, oil);
+        System.out.println("Bitte zu fahrende Strecke eingeben");
+        double distance=new Scanner(System.in).nextDouble();
+        System.out.println("Bitte Benzinpreis eingeben");
+        double price=new Scanner(System.in).nextDouble();
+
+        car.cost(distance,price);
+    }
+
+}
